a9b70f1467e85322d6222c41d171598472a2b26d
DaviFreita <davi.freitas@alunos.ufersa.edu.br>
2025-08-14 15:44:23 -0300

    update 2

--- src/CondicaoIF.java
+++ src/CondicaoIF.java
@@ -1,7 +1,7 @@
 public class CondicaoIF {
     public static void main(String[] args) {
         int idade = 20;
-
+        //se idade for maior ou igual a 18, a condição vai ser verdadeira
         if(idade >= 18 ){
             System.out.println("Você já é maior de idade");
         }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //Ola meu mundo Java
         System.out.printf("Olá Mundo");
     }
 }
