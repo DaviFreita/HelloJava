118a109f31cf16792888c4bb20bec419f7a75175
DaviFreita <davi.freitas@alunos.ufersa.edu.br>
2025-08-14 15:37:51 -0300

    My First Program in Java

---
+++ src/CondicaoIF.java
@@ -0,0 +1,9 @@
+public class CondicaoIF {
+    public static void main(String[] args) {
+        int idade = 20;
+
+        if(idade >= 18 ){
+            System.out.println("Você já é maior de idade");
+        }
+    }
+}
